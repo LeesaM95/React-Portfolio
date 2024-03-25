c09c0310d3a6c33d434d40a190d4b68b4ddfa44f
LeesaM95 <146777629+LeesaM95@users.noreply.github.com>
2024-03-24 22:03:56 -0400

    worked on AboutMe page and styling.
    worked on contact me logic for email validation. input footer icons. made a validateEmail helper

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -2,9 +2,46 @@
 // Must Include:
 // recent photo/avatar of myself
 // short bio about myself
+import '../components/css/style.css'
 
 export default function About() {
-    return{
-        
-    }
+    return(
+        <div class="container-fluid">
+            <img src="https://images2.imgbox.com/eb/af/xTykfPC8_o.png" style="height: 300px; width:300px; margin: 20px;" class="rounded float-start avii-img"></img>
+            <h2 class="display-5">About Me</h2>
+            <div class="aboutme">
+            <p>Hello! My name is Leesa. I was born and raised in a small city in central Massachusetts and have been living in Eastern Pennsylvania since 2020. 
+               I spent my childhood sketching away on loose-leaf computer paper and dreaming up fantastical stories about larger-than-life heroines
+               and great, sprawling fantasy realms (strange how I got into programming, I know). When I'm not pouring over work, I enjoy reading, writing, and playing video games.
+               <i>Sims 4</i>, <i>Stardew Valley</i>, <i>Animal Crossing</i>, and <i>A Little to The Left</i> are some of my favorite titles. I love the idea of travelling, 
+               and hope to start visiting different countries in the next couple of years.
+            </p>
+            <p>
+               Currently, I work as a Unit Secretary in an Emergency Department. My job is to field calls, 
+               faciliate communication between my emergency department providers and specialists,
+               sort through patient documents, faciliate patient transfers, and whatever else needs to be done for my providers and nurses. 
+               My doc know that if they need to get a hold of someone fast, I'm their girl. I'm <i>very</i> annoying to specialists when they
+               don't answer in a timely fashion.
+            </p>
+            <p>
+                My job can be very challenging and rather thankless at times, but I enjoy helping my providers and nurses. 
+                They work so hard and often deal with some really crummy situations and people, so, if I can make their job a little easier, I'm happy. 
+                My job centers around computers and phones, so it's no surprise that I've grown curious about how the programs I use work. I've always been interested 
+                in HTML and CSS, so it only made sense for me to further my knowledge. 
+            </p>
+            <p>
+                At the time of writing this, I'm a little less than a month away from finishing my six month accelerated Programming course hosted through UCLA. It's been
+                incredibly challenging at times, and overwhelming at others, but overall incredibly rewarding whenever I figure out something I struggled with. I'm super grateful
+                for the opportunity, but I'm definitely eager to finish up, haha.
+            </p>
+            <p>
+                I like to think I'm a genuinely likeable person who's adaptable. I can be willful, and tend to take the leadership role when necessary. I'm not the kind of person
+                who enjoys being micromanaged or treated like a child, so if I'm treated that way, expect the same kind of energy in return. I like helping people, and I enjoy being
+                someone others can rely on, but don't expect me to be a pushover. Treat me with respect and I'll treat you the same. Who knows? If you manage to unlock my Tragic Backstory™,
+                you'll find I'm generally funny with a dash of chaos.
+            </p>
+            
+            </div>
+        </div>
+    )
 }
--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -2,5 +2,83 @@
 // Must Include:
 // contact form with following fields:
 // Name, Email, Message
-
 // (Fields CANNOT be left empty. Valid email address must be used (coded via js))
+
+//import useState from React
+import { useState } from "react"
+
+//import validateEmail helper
+import { validateEmail } from '../utils/helper'
+
+//create a contact function
+function Contact() {
+    //create base variables for the form fields and set the initial values to empty strings
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [message, setMessage] = useState('')
+    const [error, setError] = useState('')
+
+    // set up an input change function that gets the value and name of each imput that triggered the change
+
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+
+        // based on what's entered, we set the state of [email, name, message]
+
+        if (inputType === 'email') {
+            setEmail(inputValue)
+        } else if (inputType === 'name') {
+            setName(inputValue)
+        } else {
+            setMessage(inputValue)
+        };
+
+        // set up the form submission. 
+        const handleFormSubmission = (e) => {
+        // 1. prevent the default behavior of form submission (refreshing the page immediately)    
+            e.preventDefault();
+
+        // 2. verify if the email is valid and if the name section is empty. if YES, display an error message.
+            if(!validateEmail(email) || !name) {
+                setError("Name or Email invalid!");
+        // 3. make sure we exit out of this block of code if something's wrong so that it can be corrected.
+                return;
+        
+            }
+        // 3b. check to see if the message is invalid. if YES, send error message
+            if(!setMessage(message)) {
+                setError("Section cannot be empty!");
+                return;
+            }
+        }
+        // 4. If everything is valid, we want to make sure that the submit form is cleared after the previous form was filled out
+            setName('');
+            setEmail('');
+            setMessage('')
+        
+    };
+};
+
+// set up html section and styling
+
+return (
+    <div class="container">
+        <div class="mb-3">
+        <label for="inputName" class="form-label">Name</label>
+        <input type="text" id="inputName" class="form-control"></input>
+        </div>
+
+        <div class="mb-3">
+        <label for="inputEmail" class="form-label"></label>
+        <input type="text" id="inputEmail" class="form-control"></input>
+        </div>
+
+        <div class="mb-3">
+        <label for="inputMessage" class="form-label"></label>
+        <input type="text" id="inputmessage" class="form-control"></input>
+        </div>
+    </div>
+)
